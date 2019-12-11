51c5da435441520db830a33090a95be2ffaac3ef
Alfredo Acosta (ClaroDom) <Alfredo_Acosta@corp.codetel.com.do>
2019-12-10 20:12:43 -0400

    Adding some classes to playground with some topics.

--- src/main/java/learn/mosh/java/Main.java
+++ src/main/java/learn/mosh/java/Main.java
@@ -5,15 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How can i call you?");
-        String userInput = sc.nextLine();
+//        Browser browser = new Browser();
+//        browser.navigate("Google");
 
-        greetUser(userInput);
-        sc.close();
+        new Employee(10_000);
+        Employee employee = new Employee(50_000,20);
+//        employee.setBaseSalary(50_000);
+//        employee.setHourlyRate(20);
+        int wage = employee.calculateWage(10);
+        employee.calculateWage();
+        System.out.println(wage);
+
+//
+//        //Procedural programming is overload a main method calling procedures... or functions on the main.
+//        int baseSalary = 50_000;
+//        int extraHours = 10;
+//        int hourlyRate = 20;
+//        System.out.println(calculateWage(baseSalary,extraHours,hourlyRate));
     }
 
-    private static void greetUser(String name){
-        System.out.println("Hi, " + name);
-    }
+//    private static int calculateWage(int baseSalary, int extraHours, int hourlyRate){
+//        return baseSalary + (extraHours * hourlyRate);
+//    }
 }
+//Static to make a member part of the class and not replicate that field at the object.
+// Non-static fields are part of the object not the class
